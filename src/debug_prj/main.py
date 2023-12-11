3bcac6198e8586a9283f8ada6f92c5c05e01913f
Martyniuk Vadym <martynyukvadum@gmail.com>
2023-12-11 23:29:02 +0200

    Update of the `debug` subproject to plot required graphs
    Created `SolarCell_new` class that accommodates all the fixes and improvements of existing `SolarCell` class
    Created separate functions to plot some graphs that confirm POC

--- src/debug_prj/main.py
+++ src/debug_prj/main.py
@@ -156,9 +156,14 @@ class SolarCell_new:
         if self.I0 is None:
             self.Rs, self.Rp, self.I0 = self.find_parameters()
 
-    def run(self, weight_arr=None):
+    def res_enum(self, weight_arr=None):
         self.current, self.Rs, self.Rp, self.approx_error = self.find_best_fit(weight_arr)
-        print("\nRs = ", self.Rs, "\nRp = ", self.Rp, "\nRS Error = ", self.approx_error)
+        print("\nError = ", self.approx_error)
+
+    def alpha_enum(self, weight_arr=None, use_one_diode_model=True):
+        self.current, self.a1, self.a2, self.approx_error = self.find_best_fit_1(weight_arr,
+                                                                                 use_one_diode_model)
+        print("\nError = ", self.approx_error)
 
     def recalc(self, Rs, Rp, G, voltage=None):
         self.G = G
@@ -221,6 +226,29 @@ class SolarCell_new:
                 arg = (V + prev_current * Rs) / self.Vt
                 new_current = self.Ipv - (V + prev_current * Rs) / Rp - exp(arg / self.a1 + lIb) - \
                               exp(arg / self.a2 + lIb) + 2 * self.I0
+                prev_current = prev_current * (1 - self.eta) + self.eta * new_current
+
+                if abs(prev_current - new_current) < self.fixed_point_method_tolerance:
+                    break
+            current.append(prev_current)
+        return current
+
+    def fixed_point_method_1(self, a1, a2, voltage=None):
+        current = [self.Ipv - self.Rs * self.Ipv / self.Rp]
+        if voltage is None:
+            voltage = self.voltage[1::] if 0 in self.voltage else self.voltage
+        else:
+            voltage = voltage[1:]
+
+        lIb = log(self.I0)
+
+        for V in voltage:
+            prev_current = current[-1]
+            while True:
+                arg = (V + prev_current * self.Rs) / self.Vt
+                new_current = self.Ipv - (V + prev_current * self.Rs) / self.Rp - \
+                              exp(arg / a1 + lIb) - \
+                              exp(arg / a2 + lIb) + 2 * self.I0
                 prev_current = prev_current * (1 - self.eta) + self.eta * new_current
 
                 if abs(prev_current - new_current) < self.fixed_point_method_tolerance:
@@ -312,6 +340,41 @@ class SolarCell_new:
         self.Rp = best_fit_rp
         return best_fit_current, best_fit_rs, best_fit_rp, best_fit_error
 
+    def find_best_fit_1(self, weight_arr=None, use_one_diode_model=True):
+        best_fit_error = 100
+        best_fit_current = None
+        best_fit_a1 = None
+        best_fit_a2 = None
+
+        for idx, a1 in enumerate(np.linspace(1, 2, self.brute_force_steps, endpoint=True)):
+            self.progress_bar(idx, self.brute_force_steps)
+
+            if not use_one_diode_model:
+                for a2 in np.linspace(1, 2, self.brute_force_steps, endpoint=True):
+                    current = self.fixed_point_method_1(a1, a2)
+                    approx_error = self.find_rmse(self.e_current, current, weight_arr)
+
+                    if approx_error < best_fit_error:
+                        best_fit_error = approx_error
+                        best_fit_current = current
+                        best_fit_a1 = a1
+                        best_fit_a2 = a2
+            else:
+                current = self.fixed_point_method_1(a1, 10 ** 10)
+                approx_error = self.find_rmse(self.e_current, current, weight_arr)
+
+                if approx_error < best_fit_error:
+                    best_fit_error = approx_error
+                    best_fit_current = current
+                    best_fit_a1 = a1
+                    best_fit_a2 = 10 ** 10
+
+        self.approx_error = best_fit_error
+        self.current = best_fit_current
+        self.a1 = best_fit_a1
+        self.a2 = best_fit_a2
+        return best_fit_current, best_fit_a1, best_fit_a2, best_fit_error
+
     @staticmethod
     def find_power(voltage, current):
         return [V * I for V, I in zip(voltage, current)]
@@ -325,6 +388,10 @@ class SolarCell_new:
                 max_power_index = index
                 max_power = power
         return max_power_index, max_power
+
+    def __str__(self):
+        return (f'Rs = {self.Rs}' + f'\nRp = {self.Rp}' + f'\na1 = {self.a1}' +
+                f'\na2 = {self.a2}' + f'\nIpv = {self.Ipv}' + f'\nI0 = {self.I0}')
 
 
 def get_data(file):
@@ -386,20 +453,32 @@ def main():
                                 is_experimental_data_provided=True,
                                 mode='overall')"""
 
-    power_1000 = KC200GT_1000.find_power(KC200GT_1000.e_voltage, KC200GT_1000.e_current)
-    weight = normalize(power_1000) ** 4
-    KC200GT_1000.run(weight)
+    print(KC200GT_1000.start_slope, KC200GT_1000.end_slope)
 
-    plt.plot(voltage_800, current_800)
+    power_1000 = np.array(KC200GT_1000.find_power(KC200GT_1000.e_voltage, KC200GT_1000.e_current))
+    weight = normalize(power_1000) ** 2
+    max_power_index, _ = KC200GT_1000.find_max_power_index(power_1000)
+    # weight[max_power_index:] = [1] * (len(weight) - max_power_index)
+    # weight[max_power_index:] = np.linspace(1, 0.5, len(weight) - max_power_index)
+    # KC200GT_1000.res_enum(weight)
+    KC200GT_1000.Rs = 0.26289609693141797
+    KC200GT_1000.Rp = 120.47888324978973
+    KC200GT_1000.alpha_enum(weight, use_one_diode_model=False)
+
+    #plt.plot(voltage_800, current_800)
     plt.plot(KC200GT_1000.voltage, KC200GT_1000.current)
+    plt.plot(KC200GT_1000.e_voltage, KC200GT_1000.e_current)
+    plt.plot(KC200GT_1000.e_voltage, weight)
 
-    current_800_new = KC200GT_1000.recalc(KC200GT_1000.Rs * 1000 / 600, KC200GT_1000.Rp * 1000 / 600, G, voltage_800)
+    # current_800_new = KC200GT_1000.recalc(KC200GT_1000.Rs * 1000 / 600, KC200GT_1000.Rp * 1000 / 600, G, voltage_800)
     # current_800_new = KC200GT_1000.recalc(KC200GT_1000.Rs, KC200GT_1000.Rp, G, voltage_800)
 
-    plt.plot(voltage_800, current_800_new)
+    #plt.plot(voltage_800, current_800_new)
+    plt.grid()
     plt.show()
 
-    print(KC200GT_1000.find_rmse(current_800, current_800_new))
+    # print(KC200GT_1000.find_rmse(current_800, current_800_new))
+    print(KC200GT_1000)
 
     """
     KC200GT_1000.run(weight)
@@ -416,5 +495,189 @@ def main():
     plt.show()"""
 
 
+def plot_slope_first():
+    with open("C:\\Users\\Martyniuk Vadym\\Desktop\\solar_panel_modeling\\input_files\\" +
+              "KC200GT_new.txt") as r_file:
+        e_voltage, e_current = get_data(r_file)
+        e_voltage = np.array(e_voltage)
+
+    area_len = floor(len(e_voltage) * 0.22)
+    k, b = np.polyfit(e_voltage[:area_len], e_current[:area_len], 1)
+
+    x_line = np.array(e_voltage[:area_len])
+    y_line = k * x_line + np.full_like(x_line, b)
+
+    print(k, b)
+
+    plt.plot(e_voltage[:area_len], e_current[:area_len], marker='.', linestyle='-')
+    plt.plot(x_line, y_line, linestyle='--')
+    plt.grid()
+    plt.xlabel('V, B')
+    plt.ylabel('I, A')
+    plt.show()
+
+
+def plot_slope_second():
+    with open("C:\\Users\\Martyniuk Vadym\\Desktop\\solar_panel_modeling\\input_files\\" +
+              "KC200GT_new.txt") as r_file:
+        e_voltage, e_current = get_data(r_file)
+        e_voltage = np.array(e_voltage)
+
+    area_len = -floor(len(e_voltage) * 0.085)
+    area_len_ext = 5 * area_len
+
+    x = e_voltage[area_len:]
+    y = e_current[area_len:]
+    V_0 = e_voltage[-1]
+
+    a_1, a_2 = np.polyfit(x, y, 1)
+    b_1, b_2, b_3 = np.polyfit(x, y, 2)
+
+    x_line = np.array(e_voltage[area_len_ext:])
+    y_line_1 = a_1 * x_line + a_2
+    y_line_2 = b_1 * x_line * x_line + b_2 * x_line + b_3
+
+    print(a_1, a_2)
+    print(b_1, b_2, b_3)
+    print(2 * b_1 * V_0 + b_2)
+
+    plt.plot(e_voltage[area_len_ext:], e_current[area_len_ext:], marker='.', linestyle='-')
+    plt.plot(x_line, y_line_1, linestyle='--')
+    plt.plot(x_line, y_line_2, linestyle='-.')
+    plt.axvline(e_voltage[area_len], linestyle='--', color='black')
+    plt.grid()
+    plt.xlabel('V, B')
+    plt.ylabel('I, A')
+    plt.show()
+
+
+def plot_power_curve_window():
+    with open("C:\\Users\\Martyniuk Vadym\\Desktop\\solar_panel_modeling\\input_files\\" +
+              "KC200GT_new.txt") as r_file:
+        e_voltage, e_current = get_data(r_file)
+        e_voltage = np.array(e_voltage)
+
+    sp = SolarCell_new(cell_num=54, Voc=32.9, Isc=8.21, a1=1.0, a2=1.2,
+                       thermal_coefficient_type='absolute', G=1000,
+                       experimental_V=e_voltage,
+                       experimental_I=e_current,
+                       is_experimental_data_provided=True,
+                       mode='overall')
+
+    power_window = normalize(np.array(sp.find_power(sp.e_voltage, sp.e_current)))
+    max_power_index, _ = sp.find_max_power_index(power_window)
+    std = np.std(e_voltage)
+    window = np.exp(-(e_voltage - e_voltage[max_power_index]) ** 2 / 2 / std)
+
+    # plt.plot(e_voltage, e_current, marker='.', linestyle='-')
+    plt.plot(e_voltage, window, linestyle='--')
+    plt.plot(e_voltage, power_window, linestyle='-')
+    plt.plot(e_voltage, power_window ** 2, linestyle='--')
+    plt.plot(e_voltage, power_window ** 3, linestyle='-.')
+    plt.plot(e_voltage, power_window ** 4, linestyle=':')
+    plt.grid()
+    plt.xlabel('V, B')
+    plt.ylabel('$w_{norm}$')
+    plt.show()
+
+
+def plot_high_order_power_curve():
+    with open("C:\\Users\\Martyniuk Vadym\\Desktop\\solar_panel_modeling\\input_files\\" +
+              "KC200GT_new.txt") as r_file:
+        e_voltage, e_current = get_data(r_file)
+        e_voltage = np.array(e_voltage)
+
+    sp = SolarCell_new(cell_num=54, Voc=32.9, Isc=8.21, a1=1.0, a2=10 ** 10,
+                       thermal_coefficient_type='absolute', G=1000,
+                       experimental_V=e_voltage,
+                       experimental_I=e_current,
+                       is_experimental_data_provided=True,
+                       mode='overall')
+
+    first_idx = 0
+    second_idx = 0
+
+    target_val = e_voltage[-1] * 0.65
+    for idx, val in enumerate(e_voltage):
+        if target_val <= val:
+            first_idx = idx
+            break
+
+    target_val = e_voltage[-1] * 0.95
+    for idx, val in enumerate(e_voltage):
+        if target_val <= val:
+            second_idx = idx
+            break
+
+    power_window = normalize(np.array(sp.find_power(sp.e_voltage, sp.e_current)))
+    max_power_index, _ = sp.find_max_power_index(power_window)
+    power_window_order_4 = power_window ** 4
+    power_window_order_4[max_power_index:] = power_window[max_power_index:] ** 0.9
+    sp.alpha_enum(power_window_order_4, use_one_diode_model=False)
+
+    plt.plot(e_voltage[first_idx:], e_current[first_idx:], linestyle='-')
+    plt.plot(sp.voltage[first_idx:], sp.current[first_idx:], linestyle='--')
+    plt.plot(e_voltage[first_idx:], power_window_order_4[first_idx:], linestyle='-.')
+    plt.axvline(e_voltage[second_idx], linestyle='--', color='black')
+
+    plt.grid()
+    plt.xlabel('V, B')
+    plt.ylabel('I, A')
+    plt.show()
+
+
+def plot_gaussian_window():
+    with open("C:\\Users\\Martyniuk Vadym\\Desktop\\solar_panel_modeling\\input_files\\" +
+              "KC200GT_new.txt") as r_file:
+        e_voltage, e_current = get_data(r_file)
+        e_voltage = np.array(e_voltage)
+
+    sp = SolarCell_new(cell_num=54, Voc=32.9, Isc=8.21, a1=1.0, a2=10 ** 10,
+                       thermal_coefficient_type='absolute', G=1000,
+                       experimental_V=e_voltage,
+                       experimental_I=e_current,
+                       is_experimental_data_provided=True,
+                       mode='overall')
+
+    first_idx = 0
+    second_idx = 0
+
+    target_val = e_voltage[-1] * 0.65
+    for idx, val in enumerate(e_voltage):
+        if target_val <= val:
+            first_idx = idx
+            break
+
+    target_val = e_voltage[-1] * 0.95
+    for idx, val in enumerate(e_voltage):
+        if target_val <= val:
+            second_idx = idx
+            break
+
+    power_window = normalize(np.array(sp.find_power(sp.e_voltage, sp.e_current)))
+    max_power_index, _ = sp.find_max_power_index(power_window)
+
+    Vmpp = e_voltage[max_power_index]
+    eta = 0.1
+    window = np.exp(-0.5 * ((e_voltage - Vmpp) / eta / Vmpp / 2) ** 2)
+
+    sp.alpha_enum(window, use_one_diode_model=False)
+
+    plt.plot(e_voltage[first_idx:], e_current[first_idx:], linestyle='-')
+    plt.plot(sp.voltage[first_idx:], sp.current[first_idx:], linestyle='--')
+    plt.plot(e_voltage[first_idx:], window[first_idx:], linestyle='-.')
+    plt.axvline(e_voltage[second_idx], linestyle='--', color='black')
+
+    plt.grid()
+    plt.xlabel('V, B')
+    plt.ylabel('I, A')
+    plt.show()
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    # plot_slope_first()
+    # plot_slope_second()
+    # plot_power_curve_window()
+    # plot_high_order_power_curve()
+    plot_gaussian_window()
